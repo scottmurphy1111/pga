168967b759029a3fe6b195728590ad68da8b8790
scottmurphy1111 <scottmurphy1111@gmail.com>
2020-12-16 23:28:24 -0500

    mobile styling

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  PGA_API: 'https://api.sportsdata.io/golf/v2/json',
+  API_KEY: '8e5dfa91af234daab820a1815e4ba2c5'
+};
+
+export const params = {
+  key: environment.API_KEY
 };
